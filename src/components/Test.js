24bb0b1fc507cefc5b8aefc820a90d69467cfe1d
Zehra Khan <93855985+coTechno@users.noreply.github.com>
2022-10-19 20:32:25 +0530

    Create Test.js

---
+++ src/components/Test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Test() {
+  return (
+    <div>
+        <p>I am learning React.</p>
+      <p>My life is getting better.</p>
+    </div>
+  )
+}
+
+export default Test
